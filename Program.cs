d71df5fed13b3fd1b0dbd3cec047107a2467e082
troubleman96 <itslugenge96@gmail.com>
2024-11-12 13:13:02 +0300

    volume uno

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program{
+
+    static void Main(string [] args){
+
+        int i = 1;
+        while(i <= 3){
+            int j = 1;
+            while(j <= 3){
+                System.Console.WriteLine(i + " " + j);
+                j++;
+            }
+            i++;
+        }
+
+    }
+}
